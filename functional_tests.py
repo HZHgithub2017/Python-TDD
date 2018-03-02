fb59586b9aa66ece13391c6f0d8be323870a6ebc
HZHGithub2017 <zhaohuiihe@sina.com>
2018-03-02 14:02:50 +0800

    这是第二章的内容
    新增了测试文件内容，变得更好用

--- functional_tests.py
+++ functional_tests.py
@@ -1,10 +1,20 @@
-
 from selenium import webdriver
-#给出webdriver的路径
-drive_path = 'C:\Program Files (x86)\Google\Chrome\Application\chromedriver\chromedriver.exe'
-#这是为了忽略浏览器提示“正受到自动化软件的控制”
-option = webdriver.ChromeOptions()
-option.add_argument('disable-infobars')
-browser = webdriver.Chrome(executable_path=drive_path, chrome_options=option)
-browser.get('http://localhost:8000')
-assert 'Django' in browser.title
+import unittest
+
+class NewVisitorTest(unittest.TestCase):
+    def setUp(self):
+        # 给出webdriver的路径
+        drive_path = 'C:\Program Files (x86)\Google\Chrome\Application\chromedriver\chromedriver.exe'
+        # 这是为了忽略浏览器提示“正受到自动化软件的控制”
+        option = webdriver.ChromeOptions()
+        option.add_argument('disable-infobars')
+        self.browser = webdriver.Chrome(executable_path=drive_path, chrome_options=option)
+        self.browser.implicitly_wait(30)
+    def tearDown(self):
+        self.browser.quit()
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        self.browser.get('http://localhost:8000')
+        self.assertIn('To-Do', self.browser.title)
+        self.fail('finish the test!')
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
