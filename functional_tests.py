2a10db6800e23ce3a05c7ba0fac032dae1639394
HZHGithub2017 <zhaohuiihe@sina.com>
2018-03-07 02:23:34 +0800

    这是第四章的内容
    完善了网站与测试，重构了网页代码

--- functional_tests.py
+++ functional_tests.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import unittest
 
 class NewVisitorTest(unittest.TestCase):
@@ -9,12 +10,26 @@ class NewVisitorTest(unittest.TestCase):
         option = webdriver.ChromeOptions()
         option.add_argument('disable-infobars')
         self.browser = webdriver.Chrome(executable_path=drive_path, chrome_options=option)
-        self.browser.implicitly_wait(30)
+        self.browser.implicitly_wait(3)
     def tearDown(self):
         self.browser.quit()
     def test_can_start_a_list_and_retrieve_it_later(self):
         self.browser.get('http://localhost:8000')
         self.assertIn('To-Do', self.browser.title)
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do',header_text)
+        #应用邀请他输入一个代办事项
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(inputbox.get_attribute('placeholder'), 'Enter a to-do item')
+        inputbox.send_keys(Keys.ENTER)
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1: Buy peacock feathers' for row in rows),"New to-do item did not appear in table"
+        )
+
         self.fail('finish the test!')
+
 if __name__ == '__main__':
     unittest.main(warnings='ignore')
--- suplists/lists/tests.py
+++ suplists/lists/tests.py
@@ -2,6 +2,7 @@ from django.test import TestCase
 from django.core.urlresolvers import resolve
 from .views import home_page
 from django.http import HttpRequest
+from django.template.loader import render_to_string
 # Create your tests here.
 class HomePageTest(TestCase):
     def test_root_url_resolve_to_home_page_view(self):
@@ -13,3 +14,6 @@ class HomePageTest(TestCase):
         self.assertTrue(response.content.startswith(b'<html>'))
         self.assertIn(b'<title>To-Do lists</title>',response.content)
         self.assertTrue(response.content.endswith(b'</html>'))
+
+        expected_html = render_to_string('home.html')
+        self.assertEqual(response.content.decode(), expected_html)
